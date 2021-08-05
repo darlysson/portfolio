d62d4d17f250d4d0e2a01908fd72459e7e74e805
darlyssonalves <dalves@alter-solutions.com>
2021-08-05 12:55:32 +0100

    Fixed image metatag issue

--- src/components/Seo/index.jsx
+++ src/components/Seo/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import image from "../../images/seo-image.png"
 
 export default function Seo() {
   const data = useStaticQuery(graphql`
@@ -23,10 +24,19 @@ export default function Seo() {
       <html lang="en" amp="true" />
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="title" content={title} />
+
       <meta property="og:url" content={url} />
-      <meta property="og:type" content="portfolio" />
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
     </Helmet>
   )
 }
